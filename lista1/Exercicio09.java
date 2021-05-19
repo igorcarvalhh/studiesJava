5186b97818d44c6e9966c8a76ded44aec93b05e5
Igor Carvalho <igorcarvalhh@gmail.com>
2021-05-19 14:32:16 -0300

    'rename'

--- lista1/Exercicio09.java
+++ lista1/Exercicio09.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Exercicio9 {
+public class Exercicio09 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double fahrenheit, celsius;
